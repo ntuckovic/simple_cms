d186b97eefaf3168f3006917d4fdeee0e4007df3
Nikola Tucković <nikola.tuckovic@styria.hr>
2016-05-07 22:50:17 +0200

    static

--- simple_cms/settings.py
+++ simple_cms/settings.py
@@ -43,10 +43,13 @@ MIDDLEWARE_CLASSES = [
 
 ROOT_URLCONF = 'simple_cms.urls'
 
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -97,7 +100,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Zagreb'
 
 USE_I18N = True
 
@@ -110,3 +113,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
--- simple_cms/urls.py
+++ simple_cms/urls.py
@@ -4,6 +4,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^authoring/', include('authoring.urls')),
+    url(r'^authoring/', include('authoring.urls', namespace='authoring')),
     url(r'^admin/', admin.site.urls),
 ]
