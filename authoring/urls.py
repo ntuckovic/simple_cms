d8304fe7e2b0decf64e91a056867a62f1a264978
Nikola Tucković <nikola.tuckovic@styria.hr>
2016-05-17 09:54:19 +0200

    content update/create views

--- authoring/urls.py
+++ authoring/urls.py
@@ -1,18 +1,35 @@
 # -*- coding: utf-8 -*-
 
 from django.conf.urls import url
-from .views import LoginView, LogoutView, DashboardView, ArticlesListView, \
-    ArticleUpdateView, ArticleCreateView
+from .views import LoginView, LogoutView, DashboardView, \
+    ArticlesListView, ArticleUpdateView, ArticleCreateView, \
+    TagsListView, TagsUpdateView, TagsCreateView, \
+    CategoryListView, CategoryUpdateView, CategoryCreateView
 
 
 urlpatterns = [
     url(r'^login/?$', LoginView.as_view(), name='login'),
     url(r'^logout/?$', LogoutView.as_view(), name='logout'),
     url(r'^dashboard/?$', DashboardView.as_view(), name='dashboard'),
+    # Articles
     url(r'^articles/(?P<page>[0-9]+)/$',
         ArticlesListView.as_view(), name='articles_list'),
     url(r'^articles/update/(?P<pk>[0-9]+)/$',
         ArticleUpdateView.as_view(), name='articles_update'),
     url(r'^articles/create/$',
         ArticleCreateView.as_view(), name='articles_create'),
+    # Tags
+    url(r'^tags/(?P<page>[0-9]+)/$',
+        TagsListView.as_view(), name='tags_list'),
+    url(r'^tags/update/(?P<pk>[0-9]+)/$',
+        TagsUpdateView.as_view(), name='tags_update'),
+    url(r'^tags/create/$',
+        TagsCreateView.as_view(), name='tags_create'),
+    # Category
+    url(r'^category/(?P<page>[0-9]+)/$',
+        CategoryListView.as_view(), name='categories_list'),
+    url(r'^category/update/(?P<pk>[0-9]+)/$',
+        CategoryUpdateView.as_view(), name='categories_update'),
+    url(r'^category/create/$',
+        CategoryCreateView.as_view(), name='categories_create'),
 ]
--- authoring/views.py
+++ authoring/views.py
@@ -16,7 +16,7 @@ from django.views.generic import FormView, RedirectView, ListView, \
     TemplateView, CreateView
 from django.views.generic.edit import UpdateView
 
-from content.models import Article
+from content.models import Article, Tag, Category
 
 
 class LoginView(FormView):
@@ -69,13 +69,13 @@ class DashboardView(AutoringLoginRequiredMixin, TemplateView):
     template_name = 'authoring/dashboard.html'
 
 
-class ArticlesListView(ListView):
+class ArticlesListView(AutoringLoginRequiredMixin, ListView):
     template_name = 'authoring/articles_list.html'
     model = Article
     paginate_by = 20
 
 
-class AuthorDetailMixin(object):
+class AuthorDetailMixin(AutoringLoginRequiredMixin):
     fields = ['title', 'content', 'published', 'category', 'tags']
     model = Article
 
@@ -94,3 +94,47 @@ class ArticleCreateView(AuthorDetailMixin, CreateView):
         user = self.request.user
         form.instance.author = user
         return super(ArticleCreateView, self).form_valid(form)
+
+
+class TagsListView(AutoringLoginRequiredMixin, ListView):
+    template_name = 'authoring/tags_list.html'
+    model = Tag
+    paginate_by = 20
+
+
+class TagsDetailMixin(AutoringLoginRequiredMixin):
+    fields = ['title', 'is_active']
+    model = Tag
+
+    def get_success_url(self):
+        return reverse('authoring:tags_list', args=[1])
+
+
+class TagsUpdateView(TagsDetailMixin, UpdateView):
+    template_name = 'authoring/tags_detail.html'
+
+
+class TagsCreateView(TagsDetailMixin, CreateView):
+    template_name = 'authoring/tags_detail_new.html'
+
+
+class CategoryListView(AutoringLoginRequiredMixin, ListView):
+    template_name = 'authoring/category_list.html'
+    model = Category
+    paginate_by = 20
+
+
+class CategoryDetailMixin(AutoringLoginRequiredMixin):
+    fields = ['title', 'parent']
+    model = Category
+
+    def get_success_url(self):
+        return reverse('authoring:categories_list', args=[1])
+
+
+class CategoryUpdateView(CategoryDetailMixin, UpdateView):
+    template_name = 'authoring/category_detail.html'
+
+
+class CategoryCreateView(CategoryDetailMixin, CreateView):
+    template_name = 'authoring/category_detail_new.html'
