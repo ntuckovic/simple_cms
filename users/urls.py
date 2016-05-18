250608f1d8327d8ede9930449f6a3bef9e660a16
Nikola Tucković <nikola.tuckovic@styria.hr>
2016-05-18 09:12:57 +0200

    profile update

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 
 from django.conf.urls import url
-from .views import LoginView, LogoutView
+from .views import LoginView, LogoutView, UpdateProfileView
 
 
 urlpatterns = [
     url(r'^login/?$', LoginView.as_view(), name='login'),
     url(r'^logout/?$', LogoutView.as_view(), name='logout'),
+    url(r'^profile/?$', UpdateProfileView.as_view(), name='profile'),
 ]
--- users/views.py
+++ users/views.py
@@ -2,7 +2,12 @@
 
 from __future__ import unicode_literals
 
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic.edit import UpdateView
+
 from simple_cms.mixins.views import LoginViewMixin, LogoutViewMixin
+
+from .models import UserProfile
 
 
 class LoginView(LoginViewMixin):
@@ -11,3 +16,20 @@ class LoginView(LoginViewMixin):
 
 class LogoutView(LogoutViewMixin):
     url = '/'
+
+
+class UpdateProfileView(LoginRequiredMixin, UpdateView):
+    template_name = 'users/profile_update.html'
+    success_url = '/users/profile/'
+    model = UserProfile
+    fields = [
+        'first_name', 'last_name', 'gender',
+        'avatar', 'date_of_birth', 'address_street',
+        'address_place', 'address_postcode',
+        'country', 'favorite_categories'
+    ]
+
+    def get_object(self, queryset=None):
+        user = self.request.user
+
+        return self.model.objects.get(user=user)
